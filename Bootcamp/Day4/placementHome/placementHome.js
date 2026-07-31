a4fa1c747d29c81474bb767ba36d663b282770b5
Amrutha Koyyalamudi <amruthakoyyalamudi@gmail.com>
2026-07-31 15:13:46 +0530

    Create placementHome.js

---
+++ Bootcamp/Day4/placementHome/placementHome.js
@@ -0,0 +1,20 @@
+## Initial 
+
+import { LightningElement } from 'lwc';
+
+export default class PlacementHome extends LightningElement {
+
+}
+
+
+## Modified
+
+import { LightningElement } from 'lwc';
+
+export default class PlacementHome extends LightningElement {
+
+    studentName = 'Amrutha';
+    rollNumber = '23pa1a1210';
+    department = 'IT';
+
+}
